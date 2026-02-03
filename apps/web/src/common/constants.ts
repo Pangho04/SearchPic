cc26c32f18912cc12a6a03b9686d851b08467f8f
이창호 <dlckdgh741@naver.com>
2026-02-04 02:56:31 +0900

    <Fix> Home Scene UI 구현 수정

--- apps/web/src/common/constants.ts
+++ apps/web/src/common/constants.ts
@@ -1,1 +1,15 @@
 export const APPLICANT_NAME = '이창호';
+
+export const STRINGS = {
+  home: {
+    header: {
+      name: APPLICANT_NAME,
+    },
+    main: {
+      content: `안녕하세요\n${APPLICANT_NAME}입니다.`,
+    },
+    footer: {
+      buttonText: '다음',
+    },
+  },
+};
--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -1,9 +1,35 @@
+import { MainButton, TextLabel } from '@searchpic/ui';
+import { useNavigate } from 'react-router-dom';
+import { STRINGS } from '@/common/constants';
+import { Header, Footer } from '@/components';
+import { ResultPath } from '@/router/Paths';
+
 export default function Home() {
+  const navigate = useNavigate();
+
+  const homeStrings = STRINGS.home;
+
+  const handleClickNextBtn = () => {
+    navigate(ResultPath);
+  };
+
   return (
-    <div className="flex flex-col min-h-screen">
-      <main className="flex flex-col flex-1 justify-center items-center p-4">
-        <h1 className="mb-6 text-3xl font-bold">사진 조회</h1>
+    <div className="flex min-h-screen flex-col">
+      <Header>
+        <TextLabel text={homeStrings.header.name} color="#1A1A1A" weight="medium" />
+      </Header>
+      <main className="relative flex flex-1 flex-row items-center justify-center gap-2.5 self-stretch p-2.5">
+        <TextLabel
+          text={homeStrings.main.content}
+          color="#111111"
+          size="28px"
+          weight="semibold"
+          additionalClasses="lg:text-[32px]"
+        />
       </main>
+      <Footer>
+        <MainButton text={homeStrings.footer.buttonText} onClick={handleClickNextBtn} />
+      </Footer>
     </div>
   );
 }
